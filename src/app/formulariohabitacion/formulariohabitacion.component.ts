d2b168ae578dd98c8c9321d050d94ed0033b8630
Mateo Jaramillo Suarez <mateoloavvio12@gmail.com>
2026-02-13 15:43:24 -0500

    se agregaron modificacioness a url vercel

--- src/app/formulariohabitacion/formulariohabitacion.component.ts
+++ src/app/formulariohabitacion/formulariohabitacion.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import {FormGroup, FormBuilder} from '@angular/forms'
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { HabitacionesService } from '../services/habitaciones.service';
+import { Habitacion } from '../interfaces/habitacion';
 
 @Component({
   selector: 'app-formulariohabitacion',
@@ -8,31 +9,53 @@ import { HabitacionesService } from '../services/habitaciones.service';
   styleUrls: ['./formulariohabitacion.component.css']
 })
 export class FormulariohabitacionComponent {
-  public formulario:FormGroup
-  public datosFormulario:any[]=[]
+  public formulario: FormGroup;
+  public cargandoRegistro = false;
+  public mensajeRegistro = '';
+  public registroExitoso = false;
 
-  public constructor(public constructorFormulario:FormBuilder,public servicio:HabitacionesService){
-    this.formulario=this.inicializarFormulario()
+  public constructor(
+    public constructorFormulario: FormBuilder,
+    public servicio: HabitacionesService
+  ) {
+    this.formulario = this.inicializarFormulario();
   }
 
-  public inicializarFormulario():FormGroup{
+  public inicializarFormulario(): FormGroup {
     return this.constructorFormulario.group({
-      nombre:[''],
-      foto:[''],
-      descripcion:[''],
-      precio:[''],
-      numeropersonas:['']
-    })
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      foto: ['', [Validators.required]],
+      descripcion: ['', [Validators.required, Validators.minLength(10)]],
+      precio: [null, [Validators.required, Validators.min(1)]],
+      numeropersonas: [null, [Validators.required, Validators.min(1), Validators.max(20)]]
+    });
   }
 
   public procesarDatos(): void {
-    let datos = this.formulario.value
-    
-    this.servicio.registrarHabitacion(datos).subscribe((respuesta)=> {
-      console.log(respuesta);
-      
-    })
-    
+    if (this.formulario.invalid || this.cargandoRegistro) {
+      this.formulario.markAllAsTouched();
+      return;
+    }
+
+    this.cargandoRegistro = true;
+    this.mensajeRegistro = '';
+    this.registroExitoso = false;
+
+    const datos = this.formulario.value as Habitacion;
+
+    this.servicio.registrarHabitacion(datos).subscribe({
+      next: () => {
+        this.registroExitoso = true;
+        this.mensajeRegistro = 'Habitación registrada correctamente.';
+        this.formulario.reset();
+        this.cargandoRegistro = false;
+      },
+      error: (error) => {
+        this.registroExitoso = false;
+        this.mensajeRegistro = error?.error?.mensaje || 'No se pudo registrar la habitación. Verifica la URL y el endpoint de tu API.';
+        this.cargandoRegistro = false;
+      }
+    });
   }
 
 }
--- src/app/services/habitaciones.service.ts
+++ src/app/services/habitaciones.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable } from 'rxjs';
+import { Habitacion } from '../interfaces/habitacion';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HabitacionesService {
 
-  public url:string="https://proyectoadesplegar.vercel.app"
+  public url: string = environment.apiUrl;
 
   constructor(public servicioHabitacion:HttpClient) { }
 
@@ -17,7 +19,7 @@ export class HabitacionesService {
     return this.servicioHabitacion.get(uri)
   }
 
-  public registrarHabitacion(datos:any):Observable<any>{
+  public registrarHabitacion(datos: Habitacion): Observable<any> {
     let endpoint = "/registrarhabitacion"
     let uri=this.url+endpoint
     return this.servicioHabitacion.post(uri,datos)
