fd6e08282eb95d737d4b3b392600f3dabd5e56f4
Mateo Jaramillo Suarez <mateoloavvio12@gmail.com>
2026-02-17 10:01:38 -0500

    Force redeploy: auth UI update

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,8 @@
 @echo off
 cd /d "c:\Users\teoja\OneDrive\Escritorio\Angularproyect"
+:: Construir el proyecto Angular
+call npx ng build --prod
+:: Subir los cambios generados
 git add -A
 git commit -m "Force redeploy: auth UI update"
 git push origin main
